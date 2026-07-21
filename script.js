b9e9b71e9b830c280c4a86ea924076395e99c88a
mattyjo-froggy <mattlanglais@proton.me>
2026-07-20 23:04:17 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const grid = document.querySelector(".grid");
 
+const cards = [...document.querySelectorAll(".card")];
+
 const leftColumn = document.createElement("div");
 leftColumn.classList.add("column");
 
 const rightColumn = document.createElement("div");
 rightColumn.classList.add("column");
+
+grid.innerHTML = "";
 
 grid.appendChild(leftColumn);
 grid.appendChild(rightColumn);
